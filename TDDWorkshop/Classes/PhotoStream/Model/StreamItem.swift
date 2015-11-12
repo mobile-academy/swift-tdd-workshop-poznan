709dd9716b084bf1b3fae8a164f55e3d079b8a79
Aleksander Zubala <alek.zubala@gmail.com>
2015-11-12 19:17:16 +0100

    Added Objective-C bridging header. Added stub classes for Photo Stream.

---
+++ TDDWorkshop/Classes/PhotoStream/Model/StreamItem.swift
@@ -0,0 +1,17 @@
+//
+// Copyright (c) 2015 Mobile Academy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class StreamItem {
+    var title: String
+    var image: UIImage
+
+    init(title: String, image: UIImage) {
+        self.title = title
+        self.image = image
+    }
+
+}
---
+++ TDDWorkshop/Classes/PhotoStream/Model/StreamItemUploader.swift
@@ -0,0 +1,8 @@
+//
+// Copyright (c) 2015 Mobile Academy. All rights reserved.
+//
+
+import Foundation
+
+class StreamItemUploader {
+}
