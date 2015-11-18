fc032d7d6097308dfed4dfc756ae5785c52d7f79
Lukasz Warchol <lwarchol@shutterstock.com>
2015-11-19 00:17:12 +0100

    Initial setup for Agenda.

--- TDDWorkshop/Classes/Agenda/AgendaViewController.swift
+++ TDDWorkshop/Classes/Agenda/AgendaViewController.swift
@@ -5,16 +5,38 @@
 
 import UIKit
 
-class AgendaViewController: UIViewController {
+class AgendaViewController: UITableViewController {
+
+    var agendaProvider: AgendaProviding!
+
+    // MARK: Initialisers
     
+    override init(style: UITableViewStyle) {
+        super.init(style: style)
+        self.agendaProvider = AgendaProvider()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.agendaProvider = AgendaProvider()
+    }
+    
+    // MARK: Lifecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // TODO: reload agenda
+    }
+
+    // MARK: Table view
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0;
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(AgendaItemCellIdentifier, forIndexPath: indexPath) as! AgendaItemCell
+        return cell;
     }
-    
 }
